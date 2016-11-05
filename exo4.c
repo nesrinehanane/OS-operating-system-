7ef32b78362b01dbd050787c48ef3df85371ccad
nesrine <nislafe@gmail.com>
2016-11-05 17:45:41 +0100

    Add exo4 final version

--- exo4.c
+++ exo4.c
@@ -7,20 +7,23 @@
 
 sem_t semaph;
 
-void affichagemessage1(void){
-   sleep(rand()%5);
+/*procedure qui affiche message "Un..."*/
+void *affichagemessage1(void){
+   sleep(rand()%1);
    sem_wait(&semaph);
    printf("Un,...");
+   sem_post(&semaph);
    pthread_exit(0);
 
 
 }
 
-
-void affichagemessage2(void){
-   sleep(rand()%5);
+/*procedure qui affiche message "Deux..."*/
+void *affichagemessage2(void){
+   sleep(rand()%1);
    sem_wait(&semaph);
    printf("Deux,...");
+   sem_post(&semaph);
    pthread_exit(0);
 
 
@@ -31,18 +34,18 @@ int main(void){
 pthread_t thread1,thread2;
 
 /*initialisation de semaphore sans nom*/
-if(sem_init(&semaph,0,0)){
+if(sem_init(&semaph,0,1)){
    perror("seminit");
    exit(EXIT_FAILURE);
    }
 
 
-
-   if(pthread_create(&thread1, NULL,  (void*)affichagemessage2,NULL)){
+/*creation du premier thread*/
+   if(pthread_create(&thread1, NULL,(void *)affichagemessage2,NULL)){
     fprintf(stderr,"thread2");
     exit(EXIT_FAILURE);
    }
-
+/*creation du second thread*/
    if(pthread_create(&thread2, NULL, (void*) affichagemessage1,NULL)){
     fprintf(stderr,"thread1");
     exit(EXIT_FAILURE);

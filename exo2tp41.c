f579f107f431426040a6932a7a4481335c8fa5da
nesrine <nislafe@gmail.com>
2016-11-22 21:44:23 +0100

    avant derniere version

--- exo2tp41.c
+++ exo2tp41.c
@@ -5,7 +5,7 @@
 #include <semaphore.h>
 
 
-#define N 100
+#define N 10
 #define NP 10
 #define NC 5
 char buffer [N];
@@ -20,29 +20,39 @@ pthread_cond_t notempty= PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex =PTHREAD_MUTEX_INITIALIZER;
 
 //Fonction exécutée par les threads producteurs
-void produce_char( void * args){
-char x;
-	if (count == N)
-		pthread_cond_wait (&notfull,&mutex);
-		pthread_mutex_lock (&mutex) ;
-		buffer [nextin] = x;
-		nextin = (nextin + 1) % N;
-		count ++;
-		pthread_mutex_unlock (&mutex) ;
-		pthread_cond_signal (&notempty);
+void produce_char( void * args ){
+int i;
+	
+	 char x= 65+rand()%27;
+        if (count == N)
+        pthread_cond_wait(&notfull,&mutex);
+        pthread_mutex_lock(&mutex);
+        buffer [nextin] = x;
+	printf("produit %c\n",buffer[nextin]);
+        nextin = (nextin + 1) % N;
+        count ++;
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&notempty);
+    
+    pthread_exit(0);
+
 }
 
 /* Fonction exécutée par les threads consommateur */
 void take(void * args) {
-char *x;
-if (count == 0)
-	pthread_cond_wait (&notempty,&mutex);
-	pthread_mutex_lock (&mutex) ;
-	*x = buffer [nextout];
-	nextout = (nextout + 1) % N;
-	count --;
-	pthread_mutex_unlock (&mutex) ;
-	pthread_cond_signal(&notfull);
+char x;
+
+        if (count == 0)
+        pthread_cond_wait(&notempty, &mutex) ;
+        pthread_mutex_lock(&mutex);
+        x = buffer [nextout] ;
+	printf("consommé %c\n", x);
+        nextout = (nextout + 1) % N;
+        count --;
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&notfull);
+    
+    pthread_exit(0);
 }
 
 
@@ -52,6 +62,18 @@ pthread_t thread_C[NC];
 
 int createP, createC;
 long i;
+int j;
+
+//inisialisation 
+/*char T[N];
+    printf("initialisation\n");
+    for (j = 0; j < N; i++) {
+	/*initialisation du tableau avec des lettres alphabetiques aleatoires 
+	65 represente le code de A %27 de 0 a 26 nb de lettre alphabetiques
+       T[j] = 65+rand()%27;
+        
+    }*/
+
 
 //creation des threads producteurs 
     for(i=0;i<NP;i++){
@@ -85,14 +107,15 @@ pthread_join(thread_P[i],NULL);
 
 }
 printf("Le main attend la fin des threads consomateurs \n");
-for(i=0;i<NC;i++){
-pthread_join(thread_C[i],NULL);
+for(j=0;j<NC;j++){
+pthread_join(thread_C[j],NULL);
 
 }
 //destruction des variables de condition
 
 pthread_cond_destroy(&notfull); 
 pthread_cond_destroy(&notempty);
+pthread_mutex_destroy(&mutex);
 
 
 pthread_exit(0);

f3b444be878b38ca0c38fb2814ec904ca0cefaeb
nesrine <nislafe@gmail.com>
2016-11-22 19:46:03 +0100

    ADD EXO2 TP 4 final version

--- exo2tp4.c
+++ exo2tp4.c
@@ -1,43 +1,46 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
-#include<semaphore.h>
+#include <semaphore.h>
+
+
 #define N 100
 #define NP 10
 #define NC 5
 char buffer [N];
 int nextin = 0, nextout = 0;
 int count = 0;
-condition notfull, notempty;
+
+pthread_cond_t notfull= PTHREAD_COND_INITIALIZER;
+pthread_cond_t notempty= PTHREAD_COND_INITIALIZER;
 
 pthread_mutex_t mutex =PTHREAD_MUTEX_INITIALIZER;
 
 
-void produce_char ( char x)
-{
-
-if (count == N)
-	cond_wait (notfull);
-	pthread_mutex_lock (&mutex) ;
-	buffer [nextin] = x;
-	nextin = (nextin + 1) % N;
-	count ++;
-	pthread_mutex_unlock (&mutex) ;
-	cond_signal (notempty);
+void produce_char( void * args){
+char x;
+	if (count == N)
+		pthread_cond_wait (&notfull,&mutex);
+		pthread_mutex_lock (&mutex) ;
+		buffer [nextin] = x;
+		nextin = (nextin + 1) % N;
+		count ++;
+		pthread_mutex_unlock (&mutex) ;
+		pthread_cond_signal (&notempty);
 }
 
-
 /* Fonction exécutée par les threads consommateur */
-void take (char *x) { // retirer un caractère du buffer
+void take(void * args) {
+char *x;
 if (count == 0)
-	cond_wait (notempty);
+	pthread_cond_wait (&notempty,&mutex);
 	pthread_mutex_lock (&mutex) ;
 	*x = buffer [nextout];
 	nextout = (nextout + 1) % N;
 	count --;
 	pthread_mutex_unlock (&mutex) ;
-	cond_signal (notfull);
+	pthread_cond_signal(&notfull);
 }
 
 
@@ -49,20 +52,20 @@ int createP, createC;
 long i;
 
     for(i=0;i<NP;i++){
-       
-        create_P=pthread_create(&thread_P[i],NULL,(void *)produce_char,buffer[i]);
-        if(create_P){
-            printf("Erreur de creation de thread %d\n",create);
+
+        createP=pthread_create(&thread_P[i],NULL,(void *)produce_char,NULL);
+        if(createP){
+            printf("Erreur de creation de thread %d\n",createP);
             exit(-1);
 
         }
 
     }
 for(i=0;i<NC;i++){
-       
-        create_C=pthread_create(&thread_C[i],NULL,(void *)take,(void *));
-        if(create_C){
-            printf("Erreur de creation de thread %d\n",create);
+
+        createC=pthread_create(&thread_C[i],NULL,(void *)take,NULL);
+        if(createC){
+            printf("Erreur de creation de thread %d\n",createC);
             exit(-1);
 
         }
@@ -86,5 +89,3 @@ pthread_exit(0);
 
 
 }
-
-
